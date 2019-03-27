11467dac6354e24550967c643500f48b10f231d5
Shinji Ezumi <ezumi@trustbank.co.jp>
2019-03-27 18:01:32 +0900

    [DDG]サンプル実装追加

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('top');
-});
+})->name('top');
+
+Route::get('/ddg', 'DdgController@index')->name('ddg');
+Route::get('/pc', 'PcController@index')->name('pc');
