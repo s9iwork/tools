b29b8ade5e14a4a9936a82bf6b13de9032c8762a
Shinji Ezumi <ezumi@trustbank.co.jp>
2019-03-29 16:07:16 +0900

    ツール名の変更、データ種別を追加（暫定）

--- resources/views/ddg/top.blade.php
+++ resources/views/ddg/top.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'ダミーデータ作成ツール')
+@section('title', 'ダミーデータ作る君')
 
 @section('content')
 	<main class="container">
@@ -9,39 +9,27 @@
 				<div class="col s12">
 					<ul class="tabs z-depth-1">
 						<li class="tab col s3">
-							<a href="#category1">アイパス系</a>
+							<a href="#category-1">個人情報系</a>
 						</li>
 						<li class="tab col s3">
-							<a href="#category2">個人情報系</a>
+							<a href="#category-2">Web系</a>
 						</li>
 						<li class="tab col s3">
-							<a href="#category3">クレカ系</a>
+							<a href="#category-3">時間系</a>
 						</li>
 						<li class="tab col s3">
-							<a href="#category4">地図系</a>
-						</li>
-						<li class="tab col s3">
-							<a href="#category5">未分類</a>
+							<a href="#category-99">未分類</a>
 						</li>
 					</ul>
 				</div>
-				<div id="">
-					<p>アイパス系</p>
-					<div>
-						<label>
-							<input class="with-gap" name="type" type="radio"/>
-							<span>ID</span>
-						</label>
-					</div>
-					<div>
-						<label>
-							<input class="with-gap" name="type" type="radio"/>
-							<span>パスワード</span>
-						</label>
-					</div>
-				</div>
-				<div id="">
+				<div id="category-1">
 					<p>個人情報系</p>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>国</span>
+						</label>
+					</div>
 					<div>
 						<label>
 							<input class="with-gap" name="type" type="radio"/>
@@ -69,18 +57,204 @@
 					<div>
 						<label>
 							<input class="with-gap" name="type" type="radio"/>
-							<span>メールアドレス</span>
+							<span>メールアドレス（存在しない）</span>
 						</label>
 					</div>
 					<div>
 						<label>
 							<input class="with-gap" name="type" type="radio"/>
-							<span>国</span>
+							<span>会社名</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>銀行口座</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>クレジットカード種類</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>クレジットカード番号</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>クレジットカード有効期限</span>
 						</label>
 					</div>
 				</div>
-				<div id="">
-					<p>地図系</p>
+				<div id="category-2">
+					<p>Web系</p>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>ID</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>パスワード</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>ドメイン名</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>トップレベルドメイン（.comなど）</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>URL</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>IPV4アドレス</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>IPV6アドレス</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>UserAgent</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>UserAgent(Chrome)</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>UserAgent(Safari)</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>UserAgent(Opera)</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>UserAgent(IE)</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>UserAgent(IE)</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>MIMEタイプ</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>拡張子</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>16進数カラーコード</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>RGBカラー</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>カラー名</span>
+						</label>
+					</div>
+				</div>
+				<div id="category-3">
+					<p>時間系</p>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>タイムゾーン</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>日付（今年）</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>日付（今月）</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>日付</span>
+						</label>
+					</div>
+				</div>
+				<div id="category-99">
+					<p>未分類</p>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>フリーテキスト</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>国コード</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>国コード(3桁)</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>通貨コード</span>
+						</label>
+					</div>
 					<div>
 						<label>
 							<input class="with-gap" name="type" type="radio"/>
@@ -90,16 +264,19 @@
 					<div>
 						<label>
 							<input class="with-gap" name="type" type="radio"/>
-							<span>軽度</span>
+							<span>経度</span>
 						</label>
 					</div>
-				</div>
-				<div id="">
-					<p>未分類</p>
 					<div>
 						<label>
 							<input class="with-gap" name="type" type="radio"/>
-							<span>軽度</span>
+							<span>ISBN-13(書籍コード)</span>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input class="with-gap" name="type" type="radio"/>
+							<span>ISBN-10</span>
 						</label>
 					</div>
 				</div>
