13d0f3d2dfff8bd93c9e92605f3d706d65c90763
shinjiezumi <shinjiezumi.work@gmail.com>
2021-02-28 15:40:09 +0900

    ideヘルパ追加

--- src/app/Ddg/Models/DdgCategory.php
+++ src/app/Ddg/Models/DdgCategory.php
@@ -2,8 +2,29 @@
 
 namespace App\Ddg\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Ddg\Models\DdgCategory
+ *
+ * @property int $id
+ * @property string $name
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection|DdgDataType[] $dataTypes
+ * @property-read int|null $data_types_count
+ * @method static Builder|DdgCategory newModelQuery()
+ * @method static Builder|DdgCategory newQuery()
+ * @method static Builder|DdgCategory query()
+ * @method static Builder|DdgCategory whereCreatedAt($value)
+ * @method static Builder|DdgCategory whereId($value)
+ * @method static Builder|DdgCategory whereName($value)
+ * @method static Builder|DdgCategory whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class DdgCategory extends Model
 {
 	/**
@@ -18,7 +39,7 @@ class DdgCategory extends Model
 	 */
 	public function dataTypes()
 	{
-		return $this->hasMany('App\DdgDataType', 'category_id');
+		return $this->hasMany(DdgDataType::class, 'category_id');
 	}
 
 }
