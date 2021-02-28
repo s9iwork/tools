839cf7b03d9215e44fe7506e50afe887abfe521b
shinjiezumi <shinjiezumi.work@gmail.com>
2021-02-28 14:50:21 +0900

    docker追加

---
+++ src/scripts/init.sh
@@ -0,0 +1,16 @@
+php composer.phar install
+
+cp .env.example .env
+php artisan key:generate
+
+mkdir -p storage/framework/cache
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+
+php composer.phar dump-autoload
+
+php artisan migrate --seed
+APP_ENV=testing php artisan migrate
+
+npm install
+npm run dev
