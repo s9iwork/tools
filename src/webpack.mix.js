a66463dbe1ded8a358900df8eebf6593b72d9f73
shinjiezumi <shinjiezumi.work@gmail.com>
2021-06-05 21:28:01 +0900

    Enable hot reload

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -12,4 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js').vue()
-   .sass('resources/sass/app.scss', 'public/css');
+	.sass('resources/sass/app.scss', 'public/css')
+	.browserSync({
+		proxy: 'web',
+		open: false
+	})
+	.version();
