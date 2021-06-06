077985ab2500dd0344dc4b6bd6c16aae651d354a
shinjiezumi <shinjiezumi.work@gmail.com>
2021-06-06 21:53:06 +0900

    Delete old views

--- src/resources/js/ddg/app.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-
-Vue.component('loader', require('../components/Loader').default);
-Vue.component('ddg-top', require('./Top').default);
--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -12,9 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js').vue()
-	.sass('resources/sass/app.scss', 'public/css')
-	.browserSync({
-		proxy: 'web',
-		open: false
-	})
-	.version();
+  .browserSync({
+    proxy: 'web',
+    open: false,
+  })
+  .version();
