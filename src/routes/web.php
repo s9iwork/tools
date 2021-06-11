178efcf44d3f807a589740522f9f94df7ad803b5
shinjiezumi <shinjiezumi.work@gmail.com>
2021-06-11 22:43:41 +0900

    Fix unittest on github actions

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,9 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-// FIXME Github Actions上ではこけるので確認する
-//Route::get('/{any?}', "AppController@index")->where('any', '.*');
-
-Route::get('/', "AppController@index");
-Route::get('/ddg', "AppController@index");
-
+Route::get('/{any?}', "AppController@index")->where('any', '.*');
