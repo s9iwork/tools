c0fd8c1d75a436ac2b8b96e1521d6d7f801dd056
shinjiezumi <shinjiezumi.work@gmail.com>
2021-06-21 21:07:10 +0900

    Add tool description component

---
+++ src/resources/js/components/ToolDescription.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <div class="col s12">
+      <h2 class="aboutSubject">
+        このツールについて
+      </h2>
+      <div>
+        {{ description }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToolDescription',
+  props: {
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.aboutSubject {
+  font-size: 1.5rem;
+  border-left: solid 5px #ee6e73;
+  padding-left: 5px;
+  color: #ee6e73;
+}
+</style>
