7a924189314f1edb822de66103b4e7f53ce7b964
shinjiezumi <shinjiezumi.work@gmail.com>
2021-06-29 22:03:32 +0900

    Implement ams calculate api

--- src/app/Ams/Services/AmsService.php
+++ src/app/Ams/Services/AmsService.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Ams\Services;
 
+use App\Ams\Constants\AmsConstant;
+
 /**
  * Class AmsService.
  */
@@ -14,7 +16,31 @@ class AmsService implements IAmsService
      */
     public function calc(array $params = []): array
     {
-        // TODO 実装する
-        return $params;
+        $totalAmount = 0;
+        $transitionHistory = [];
+        $breakdown = [];
+
+        for ($i = 0; $i < AmsConstant::OPERATED_YEAR; $i++) {
+            // 資産ごとに運用額を計算
+            foreach ($params as $asset) {
+                $yearAmount = floor($asset['amount'] + ($asset['amount'] * $asset['yield'] / 100));
+
+                // 資産内訳用
+                if (!isset($breakdown[$asset['type']])) {
+                    $breakdown[$asset['type']] = 0;
+                }
+                $breakdown[$asset['type']] += $yearAmount;
+                $totalAmount += $yearAmount;
+            }
+
+            // 資産推移用
+            $transitionHistory[] = $totalAmount;
+        }
+
+        return [
+            'total_amount' => $totalAmount,
+            'transition_history' => $transitionHistory,
+            "breakdown" => $breakdown,
+        ];
     }
 }
