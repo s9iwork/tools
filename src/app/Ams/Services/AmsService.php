87ec3c136e8699ff208941866cdd27174acbd751
shinjiezumi <shinjiezumi.work@gmail.com>
2021-07-03 14:07:02 +0900

    Fix bug

--- src/app/Ams/Services/AmsService.php
+++ src/app/Ams/Services/AmsService.php
@@ -23,19 +23,23 @@ class AmsService implements IAmsService
         $year = date("Y");
         for ($i = 0; $i < AmsConstant::OPERATED_YEAR; $i++) {
             $year++;
+            $yearAmount = 0;
 
-            // 資産ごとに運用額を計算
+            // 資産ごとに資産額を計算
             foreach ($params as $asset) {
-                $baseAmount = $breakdown[$asset['type']] ?? $asset['amount'];
-                $yearAmount = floor($baseAmount + ($baseAmount * $asset['yield'] / 100));
+                // 現在の資産額に今年積立分を加算に利回り反映
+                $baseAmount = $breakdown[$asset['type']] ?? 0;
+                $baseAmount += $asset['amount'];
+                $assetAmount = floor($baseAmount + ($baseAmount * $asset['yield'] / 100));
 
                 // 資産内訳用
-                $breakdown[$asset['type']] = $yearAmount;
-                $totalAmount += $yearAmount;
+                $breakdown[$asset['type']] = $assetAmount;
+                $yearAmount += $assetAmount;
             }
 
             // 資産推移用
-            $transitionHistory[$year] = $totalAmount;
+            $transitionHistory[$year] = $yearAmount;
+            $totalAmount += $yearAmount;
         }
 
         return [
