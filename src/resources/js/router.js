b30138c8335e6cca1d008286ee31e49d870f168e
shinjiezumi <shinjiezumi.work@gmail.com>
2021-07-03 16:07:50 +0900

    Fix readme and description

--- src/resources/js/router.js
+++ src/resources/js/router.js
@@ -13,7 +13,7 @@ const routes = [
     component: Top,
     meta: {
       title: 'Webツール',
-      description: 'システムエンジニア、プログラマー向けに様々な自作Webツールを提供しています。',
+      description: 'Web開発や生活の中で用途がありそうなツールをWebサービス化しています。',
       favicon: './img/favicon.png',
     },
   },
@@ -22,7 +22,7 @@ const routes = [
     component: Ddg,
     meta: {
       title: 'ダミーデータジェネレーター｜Webツール',
-      description: '様々なテストデータ、ダミーデータを作成するツールです。',
+      description: '名前や住所などの様々なテストデータ、ダミーデータを作成するツールです。',
       favicon: './img/ddg/favicon.png',
     },
   },
@@ -31,7 +31,7 @@ const routes = [
     component: Ams,
     meta: {
       title: '資産運用シミュレーター｜Webツール',
-      description: '毎年の投資額と利回りを元に20年運用した場合の資産額を計算するツールです',
+      description: '毎年の投資額と利回りを元に20年運用した場合の資産額を計算するツールです。',
       favicon: './img/ams/favicon.png',
     },
   },
