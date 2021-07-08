3176f834ba6084ccc7641527613ddc3a1fea47a0
shinjiezumi <shinjiezumi.work@gmail.com>
2021-07-08 21:34:08 +0900

    Add sample form

--- src/resources/js/pages/Cmc.vue
+++ src/resources/js/pages/Cmc.vue
@@ -4,13 +4,36 @@
     <main slot="main" class="container content">
       <!-- TODO -->
       <ToolDescription description="カラムモデルコンバーター"/>
+      <!-- フォーム -->
+      <div class="row formContainer">
+        <div class="col s12">
+          <h2>フォーム</h2>
+          <form>
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea id="text" class="materialize-textarea" v-model="text"></textarea>
+                <label for="text">カラム一覧</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s6">
+                <input type="text" v-model="columnName">
+                <label>カラム名</label>
+              </div>
+              <div class="input-field col s6">
+                <input type="text" v-model="columnType">
+                <label>型</label>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </main>
     <Footer slot="footer"/>
   </PageContainer>
 </template>
 
 <script>
-import M from 'materialize-css';
 import PageContainer from '../components/PageContainer';
 import ToolDescription from '../components/ToolDescription';
 import Header from '../components/Header';
@@ -24,15 +47,29 @@ export default {
     ToolDescription,
     Footer,
   },
-  mounted() {
-    const elems = document.querySelectorAll('select');
-    M.FormSelect.init(elems, {});
+  data() {
+    return {
+      text: '',
+      columns: '',
+    };
   },
+
 };
 </script>
 
 <style scoped>
+h2 {
+  font-size: 1.5rem;
+  border-left: solid 5px #ee6e73;
+  padding-left: 5px;
+  color: #ee6e73;
+}
+
 .content {
   flex: 1 0 auto
+}
+
+.formContainer {
+  margin-top: 2rem;
 }
 </style>
